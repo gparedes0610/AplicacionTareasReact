b12c4a47cdfe850fd3048c31d3a8136ae60a2d75
gparedes0610 <paredesgustavo.0610@gmail.com>
2021-11-02 11:34:00 -0500

    agrege react y bootstrap con vitejs

---
+++ aplicaciontareasreact/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h2>Hola perros</h2>
+      <button className="btn btn-primary">asdad</button>
+    </>
+  );
+}
+
+export default App;
